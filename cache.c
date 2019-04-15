f8b5bce7e4329e545dabbe0853c4b585e6563e0b
Jacob <mattoxjr@g.cofc.edu>
2019-04-15 17:31:39 -0400

    skeleton c for a loop and clock

--- cache.c
+++ cache.c
@@ -3,7 +3,16 @@
 
 
 int main( int argc, char *argv[] ) {
+    clock_t start_t, stop_t;
+    unsigned long i;
 
+    start_t = clock();
+    for(i = 0; i < 4000000000; i++){
+        //dostuff
+    }
+    stop_t = clock();
+
+    printf("This is how long that number loop takes: %ld \n", (stop_t-start_t)/CLOCKS_PER_SEC);
 
 
     return 0;
