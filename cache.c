205dcea4e584c3de3ab82fc0f93d4f6283e6f0e4
Jacob <mattoxjr@g.cofc.edu>
2019-04-18 12:47:12 -0400

    changed cache

--- cache.c
+++ cache.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 
+#define ITERATIONS 400000
+
 
 int main( int argc, char *argv[] ) {
     clock_t start_t, stop_t;
     unsigned long i;
-    char myArray[4000000] = { 'a' };
-    for(i = 0; i < 4000000; i++){
+    char myArray[ITERATIONS];
+    for(i = 0; i < ITERATIONS; i++){
         myArray[i] = 'a';
         
     }
 
-    for(i = 0; myArray[i] == 'a'; i++){
+    for(i = 0; i < ITERATIONS; i++){
         start_t = clock();
-        myArray[i];
         stop_t = clock();
-        printf("%ld: This is how long that number loop takes: %ld in clock cycles \n", i, stop_t-start_t);
+        printf("%ld: This is how long that null access takes: %ld in clock cycles \n", i, stop_t-start_t);
 
         //dostuff
     }
